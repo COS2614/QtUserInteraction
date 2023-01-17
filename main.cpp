de0555be098a1d6e6af4512cb14d2238bf7f08a6
mitchcamza <192mitch88@gmail.com>
2023-01-17 15:39:00 +0200

    Get user name and display greeting

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QInputDialog>
+#include <QMessageBox>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    bool ok;
+    QString name = QInputDialog::getText(0, "Name", "Enter your name:", QLineEdit::Normal, "", &ok);
+    if (ok && !name.isEmpty())
+    {
+        QMessageBox::information(0, "Hello", "Hello, " + name);
+    }
+
     return a.exec();
 }
